dc4543cffae72dab516a33f02dc66030c7a82da2
Stephen-Roach <stephenroach21@gmail.com>
2021-03-25 22:17:39 -0500

    added routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello Humans</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+      </Switch>
+    </Router>
   );
 }
 
