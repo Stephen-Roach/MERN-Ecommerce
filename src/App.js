cd895f9ab06fbd0b1b7d466d079bed1de2a89ce0
Stephen-Roach <stephenroach21@gmail.com>
2021-03-25 22:50:14 -0500

    added ant design & nav bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
+import Header from './components/nav/Header';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/login' component={Login} />
